3c1088590fe6c031324eb10eb7012f40d3e137e8
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-16 15:45:01 +1000

    Create Main.php

---
+++ src/skyss0fly/LoginStreak/Main.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace skyss0fly\LoginStreak;
+
+use pocketmine\{plugin\PluginBase, event\Listener, event\player\PlayerJoinEvent};
+use skyss0fly\LoginStreak\StreakManager;
+
+class Main extends PluginBase implements Listener {
+
+}
