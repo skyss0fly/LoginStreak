44f090aba9a468737b9504eb445b54a845acf47f
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-17 16:40:28 +1000

    Create StreakManager.php

---
+++ src/skyss0fly/LoginStreak/StreakManager.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace skyss0fly\LoginStreak;
+use skyss0fly\LoginStreak\Main;
+class StreakManager extends Main {
+
+public function addStreak(): void {
+$player = $this->Main()->onJoin($player);
+
+}
+
+public function deductStreak():void {
+$player = $this->Main()->onJoin($player);
+
+}
+public function getData():void {
+$player = $this->Main()->onJoin($player);
+
+}
+
+}
